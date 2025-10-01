815d953437fec549e9b327cbf37ff40afa848669
Edu <educoppi8@gmail.com>
2025-10-01 17:22:53 -0300

    header inicial

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,10 @@
+import Logo from "../Logo/Index";
 import styles from "./styles.module.css"
 
 export default function Header() {
     return (
-        <>
-        
-        </>
+        <header>
+            < Logo/>
+        </header>
     );
 }
