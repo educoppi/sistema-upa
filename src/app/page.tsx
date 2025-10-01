931f99f61c6801e4be2d4460e43dba40c411f4ab
Eduardo Lombardo Fuzato <128429561+edu1107zera@users.noreply.github.com>
2025-10-01 17:23:24 -0300

    BUTTON

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Header from "@/components/Header";
+import Botao from "@/components/Header/Button";
+
+
 
 export default function Home() {
   return (
     <>
     < Header/>
+     < Botao/>
     </>
   );
 }
---
+++ src/components/Header/Button/index.tsx
@@ -0,0 +1,13 @@
+import Styles from './Styles.module.css';
+
+function Button() {
+  return (
+ 
+        <button type="button" className={Styles.button}>
+          Enviar
+        </button>
+     
+  );
+}
+
+export default Button;
