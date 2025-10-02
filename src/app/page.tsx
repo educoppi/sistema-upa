5c995a50dc0afff1c97538a95fb2bcc97d624381
Edu <educoppi8@gmail.com>
2025-10-02 17:18:50 -0300

    iniciando o login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Link from "next/link";
+import style from "./page.module.css";
 
 export default function Home() {
   return (
     <>
-        <Link href="/Views/Doctor">Login</Link>
+        <Link className={style.link} href="/Views/Doctor">Login</Link>
+
+        <div className={style.container}>
+          
+        </div>
     </>
   );
 }
