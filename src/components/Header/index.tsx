34fab18ed083fd44b9e697aa206a272d379e56ed
Edu <educoppi8@gmail.com>
2025-10-02 14:44:36 -0300

    header icon e cor de fundo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.css"
 
 export default function Header() {
     return (
-        <header>
+        <header className={styles.header}>
             < Logo/>
         </header>
     );
--- src/components/Logo/Index.tsx
+++ src/components/Logo/Index.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.css"
 export default function Logo() {
     return (
         <div className={styles.logo}>
-            <img className={styles.imagem} src="https://tse2.mm.bing.net/th/id/OIP.PUvYnhSdkXWJfT0nEHFFVwHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" alt="" />
+            <img className={styles.imagem} src="images/logo.png" alt="" />
             <h3>UPA</h3>
             <h5>Unidade de Pronto Atendimento</h5>
         </div>
