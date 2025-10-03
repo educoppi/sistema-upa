be9082acfba6063d9c55bc253e09eb61cec41c8c
Rafael Crempe <rafaelcrempe@gmail.com>
2025-10-03 16:37:30 -0300

    pagina da farmacia

--- src/app/Views/Farmacia/page.tsx
+++ src/app/Views/Farmacia/page.tsx
@@ -1,12 +1,15 @@
 'use client'
+import styles from '@/app/Views/Farmacia/styles.module.css'
 import { useState } from "react";
 import { Header } from "@/components/Header";
 import Tab from "@/components/Tab";
 import TabGroup from "@/components/TabGroup";
+import TextField from "@/components/TextField";
+import Button from '@/components/Button';
 
 export default function Farmacia() {
 
-const tabs = [{"label": "ESTOQUE", "value": "estoque"}, {"label": "SOLICITAÇÕES", "value" : "solicitacoes"}, {"label": "VENCIMENTOS", "value" : "vencimentos"}]
+  const tabs = [{ "label": "ESTOQUE", "value": "estoque" }, { "label": "SOLICITAÇÕES", "value": "solicitacoes" }, { "label": "VENCIMENTOS", "value": "vencimentos" }]
 
   return (
     <>
@@ -14,6 +17,19 @@ const tabs = [{"label": "ESTOQUE", "value": "estoque"}, {"label": "SOLICITAÇÕE
       <TabGroup tabs={tabs} name="farmacia" />
 
       <h2>CONTROLE DE MEDICAMENTOS</h2>
+
+      <div className={styles.form}>
+        <TextField label="Nome" />
+        <TextField label="Dosagem" />
+
+
+
+        <TextField label="Quantidade" />
+        <TextField label="Vencimento" />
+
+        <Button>CADASTRAR</Button>
+      </div>
+
 
 
 
