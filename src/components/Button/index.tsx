32acf5d96ead46790d514ee0f53dead9f9cb51d6
Eduardo Lombardo Fuzato <128429561+edu1107zera@users.noreply.github.com>
2025-10-06 15:18:17 -0300

    atualizaçoes medico

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,20 +1,27 @@
 import styles from './styles.module.css';
+import { CSSProperties } from "react";
+
 
 type ButtonProps = {
   children: React.ReactNode;
   onClick?: () => void;
+  style?: CSSProperties;      // precisa estar aqui
+  className?: string;         // precisa estar aqui
 };
 
-function Button({ children, onClick }: ButtonProps) {
+function Button({ children, onClick, style, className }: ButtonProps) {
   return (
-    <button 
-      type="button" 
-      className={styles.button}
+    <button
+      type="button"
+      className={`${styles.button} ${className ?? ""}`}
       onClick={onClick}
+      style={style}          // repassa style
     >
       {children}
     </button>
   );
 }
+
+
 
 export default Button;
